bcb913912c0f96b361c49a414b5dd9ea3e44e1d6
Lyrwhitt <47979697+Lyrwhitt@users.noreply.github.com>
2023-09-11 19:15:46 +0900

    [Update] [임시]스킬로인한 Ball클래스 수정
    필요한 부분을 넣엇습니다

--- RescueAnimals/Assets/Scripts/Entities/Animals/Beagle.cs
+++ RescueAnimals/Assets/Scripts/Entities/Animals/Beagle.cs
@@ -5,12 +5,23 @@ using EnumTypes;
 
 public class Beagle : Animal, IAnimalBehaviour
 {
+    private float _reinforceTime = 3f;
+
     public void OnResqueMove()
     {
 
     }
     public void OnResqueEffect(Player player)
     {
+        for(int i = 0; i < player.balls.Count; i++)
+        {
+            player.balls[i].OnPiercingMode(true, _reinforceTime);
+        }
+    }
 
+    public void OnReinforced()
+    {
+        reinforceLevel += 1;
+        _reinforceTime += 0.3f;
     }
 }
