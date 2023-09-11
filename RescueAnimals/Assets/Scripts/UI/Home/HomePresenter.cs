c41cd68b4045f9ba60737e1c0bcc02404aa15965
HYEONJI LEE <8976guswl@naver.com>
2023-09-11 12:48:20 +0900

    [Update] HomeScene UI 추가 및 로직 변경

--- RescueAnimals/Assets/Scripts/UI/Home/HomePresenter.cs
+++ RescueAnimals/Assets/Scripts/UI/Home/HomePresenter.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 
 public class HomePresenter : MonoBehaviour
@@ -16,11 +17,53 @@ public class HomePresenter : MonoBehaviour
     private void Start()
     {
         _view.OnGameStartClicked += LoadGame;
+        _view.OnUpgradeOpen += UpgradePanelOpen;
+        _view.OnRankOpen += RankPanelOpen;
+        _view.OnPlayerOpen += PlayerPanelOpen;
+        _view.OnPanelClose += PanelClose;
     }
 
     private void LoadGame()
     {
         StartCoroutine(LoadGameSceneAsync());
+    }
+    
+    private void UpgradePanelOpen()
+    {
+        ActivatePanel(_view.panels, _view.upgradePanel);
+    }
+
+    private void RankPanelOpen()
+    {
+        ActivatePanel(_view.panels, _view.rankPanel);
+    }
+
+    private void PlayerPanelOpen()
+    {
+        ActivatePanel(_view.panels, _view.playerPanel);
+    }
+
+    private void PanelClose()
+    {
+        foreach (var panel in _view.panels)
+        {
+            panel.SetActive(false);
+        }
+    }
+
+    private void ActivatePanel(GameObject[] panels, GameObject targetPanel)
+    {
+        foreach (var panel in panels)
+        {
+            if (panel == targetPanel)
+            {
+                panel.SetActive(true);
+            }
+            else
+            {
+                panel.SetActive(false);
+            }
+        }
     }
 
     private IEnumerator LoadGameSceneAsync()
