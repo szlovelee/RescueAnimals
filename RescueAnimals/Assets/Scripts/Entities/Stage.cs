a0b22cdfd36ae2ffda3cbefb7ec43694158889f7
김대열 <ajduf7180@gmail.com>
2023-09-11 13:43:54 +0900

    [Add/Update] 블록 생성처리 작업 (-대열)
    BlockGenerator 에서 패턴에 맞는 블록 생성하도록 정의
    Stage 에서 blockGenerator에게 블록 생성 위임

--- RescueAnimals/Assets/Scripts/Entities/Stage.cs
+++ RescueAnimals/Assets/Scripts/Entities/Stage.cs
@@ -1,35 +1,95 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
-using UnityEngine;
+using System.Linq;
+using Entities.BlockGenerators;
 using EnumTypes;
+using UnityEngine;
+using Random = UnityEngine.Random;
 
-public class Stage : MonoBehaviour
+namespace Entities
 {
-    public StageType stage;
-
-    public List<Block> bricks;
-    public List<Animal> animals;
-
-    [SerializeField]
-    private float bricksGenTime;
-
-    public void CalcAnimalPercentage()
+    [CreateAssetMenu(menuName = "Stage")]
+    public class Stage : ScriptableObject
     {
+        public static AnimalType[] AnimalTypes = (AnimalType[])Enum.GetValues(typeof(AnimalType));
 
-    }
+        public static BlockPattern[] BlockPatterns =
+            (BlockPattern[])Enum.GetValues(typeof(BlockPattern));
 
-    public void CalcBrickPercentage()
-    {
+        public static BlockType[] BlockTypes = (BlockType[])Enum.GetValues(typeof(BlockType));
 
-    }
+        [SerializeField] public BlockGenerator blockGenerator;
+        public List<KeyValuePair<BlockType, Vector2>> BlockPositions;
+        [SerializeField] private GameObject _prefab;
+        public List<Animal> animals;
+        public StageType stage = StageType.None;
+        private Dictionary<AnimalType, float> _animalPercentages;
+        private Dictionary<BlockType, float> _brickPercentages;
+        public float BricksGenTime => CalcBrickGenTime();
 
-    public void OnClearStage()
-    {
+        public bool IsClear => animals.Count <= 0;
 
-    }
-    
-    public void CreateBricks()
-    {
+        private void OnEnable()
+        {
+            CreateBlocks();
+            CreateAnimals();
+        }
 
+        private float CalcBrickGenTime()
+        {
+            var time = 100f - (float)stage;
+            return time < 20 ? 20 : time;
+        }
+
+        public AnimalType CalcAnimalPercentage()
+        {
+            //todo calculate animal percentage and return AnimalType
+            return AnimalType.Beagle;
+        }
+
+        public BlockType CalcBlockPercentage()
+        {
+            //todo how to set in integer...
+            var randomValue = Random.Range(0, (int)stage);
+            foreach (var block in BlockTypes)
+            {
+                if ((int)block > randomValue)
+                    return block;
+            }
+
+            return BlockTypes[^1];
+        }
+
+        public void OnClearStage()
+        {
+            CalcAnimalPercentage();
+            CreateBlocks();
+            CreateAnimals();
+        }
+
+
+        public void CreateBlocks()
+        {
+            BlockPositions = blockGenerator
+                .Generate()
+                .Select(vector2 =>
+                {
+                    var block = CalcBlockPercentage();
+                    return new KeyValuePair<BlockType, Vector2>(block, vector2);
+                }).ToList();
+        }
+
+
+        private void CreateAnimals()
+        {
+        }
+
+        public void InstantiateBlocks()
+        {
+            foreach (var (blockType, position) in BlockPositions)
+            {
+                Instantiate(_prefab, position, Quaternion.identity);
+            }
+        }
     }
 }
