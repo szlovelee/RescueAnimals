7d621c1827f1017e3ad5bda8ac2a37f9474716d4
김대열 <ajduf7180@gmail.com>
2023-09-12 00:28:29 +0900

    [Update] 게임 매니저에서 벽 생성하는 함수 추가 (-대열)

--- RescueAnimals/Assets/Scripts/Managers/GameManager.cs
+++ RescueAnimals/Assets/Scripts/Managers/GameManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Drawing;
 using Entities;
 using UnityEngine;
 using Util;
@@ -7,14 +8,22 @@ public class GameManager : Singleton<GameManager>
 {
     public Stage currentStage;
     public int score;
-    
+    [SerializeField] private GameObject wallPrefab;
+    private Camera cam;
+
     public void ChangeStage(Stage stage)
     {
         currentStage = stage;
     }
 
+    private void Awake()
+    {
+        cam = Camera.main;
+    }
+
     private void Start()
     {
+        MakeWalls();
         SetBlockStartPosition();
         currentStage.InstantiateObjects();
     }
@@ -22,13 +31,48 @@ public class GameManager : Singleton<GameManager>
     private void SetBlockStartPosition()
     {
         //todo Be camera in member variable 
-        Camera cam = Camera.main;
-        var worldRect = cam.ViewportToWorldPoint(new Vector3(1, 1));
-        var contentWidth = currentStage.BoxScale.x * currentStage.maxCol;
 
-        var startPosition = new Vector2(
-            x: -contentWidth * currentStage.BoxScale.x + currentStage.BoxScale.x * 0.5f,
-            y: worldRect.y * 1 * 0.5f); //set 3 / 4
-        currentStage.SetStartPosition(startPosition);
+        if (cam != null)
+        {
+            var worldRect = cam.ViewportToWorldPoint(new Vector3(1, 1));
+            var contentWidth = currentStage.BoxScale.x * currentStage.maxCol;
+
+            var startPosition = new Vector2(
+                x: -contentWidth * currentStage.BoxScale.x + currentStage.BoxScale.x * 0.5f,
+                y: worldRect.y * 1 * 0.5f); //set 3 / 4
+            currentStage.SetStartPosition(startPosition);
+        }
+    }
+
+    private void MakeWalls()
+    {
+        if (cam == null) return;
+
+        var worldRect = cam.ViewportToWorldPoint(new Vector3(1, 1));
+        var width = worldRect.x * 2;
+        var height = worldRect.y * 2;
+
+        var widths = new[] { 1, width, 1, width };
+        var heights = new[] { height, 1, height, 1 };
+
+        //position 0이어야 Transform.scale: 1 일 때, -0.5,+0.5 씩 확장함.
+        var startXList = new[] { worldRect.x, 0, -worldRect.x, 0 };
+        var startYList = new[] { 0, -worldRect.y, 0, worldRect.y };
+        //유니티 좌표계 (x, y) 원점은 정가운데, 1사분면 : (+,+), 2사분면 : (-, +), 3: (-, -), 4:(+, -)
+        var dx = new[] { 1, 0, -1, 0 };
+        var dy = new[] { 0, -1, 0, 1 };
+        // list에 담긴 벽들의 정보
+        // [0]  오른쪽 수직선
+        // [1]  아래쪽 수평선
+        // [2]  왼쪽 수직선
+        // [3]  위쪽 수평선  
+        for (var i = 0; i < startXList.Length; i++)
+        {
+            Vector2 position = new(
+                x: startXList[i] + widths[i] * 0.5f * dx[i], // 너비의 절반 이동해서 피봇값 상쇄 offsetX
+                y: startYList[i] + heights[i] * 0.5f * dy[i]); // 높이의 절반 이동해서 피봇값 상쇄 offsetY 
+            var go = Instantiate(wallPrefab, position, Quaternion.identity);
+            go.transform.localScale = new Vector2(widths[i], heights[i]);
+        }
     }
 }
