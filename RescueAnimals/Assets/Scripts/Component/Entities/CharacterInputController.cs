c4fa99f9fa087209a291669d7e59feb8f58dedd7
김대열 <ajduf7180@gmail.com>
2023-09-11 22:33:57 +0900

    [Update] InputController UI에서 동작 안하도록 수정(-대열)

--- RescueAnimals/Assets/Scripts/Component/Entities/CharacterInputController.cs
+++ RescueAnimals/Assets/Scripts/Component/Entities/CharacterInputController.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 namespace Entities
@@ -7,7 +8,7 @@ namespace Entities
     public class CharacterInputController : MonoBehaviour
     {
         private Camera _camera;
-
+        private bool _isPointerOverUI = false;
         private event Action<Vector2> OnTouchPressEvent;
         private event Action<Vector2> LookEvent; // todo manager manage this event (calculate in manager) 
         private event Action HitBallEvent; // todo manager manage this event (calculate in manager)
@@ -19,11 +20,21 @@ namespace Entities
 
         public void OnTouchPosition(InputValue value)
         {
+            if (_isPointerOverUI)
+            {
+                return;
+            }
+
             var screenPos = value.Get<Vector2>();
             var worldPos = _camera.ScreenToWorldPoint(screenPos);
             Debug.Log($"screenPos is {screenPos.x}, {screenPos.y}");
             Debug.Log($"InvokeEvent::({worldPos.x}, {worldPos.y})");
             OnTouchPressEvent?.Invoke(worldPos);
+        }
+
+        private void Update()
+        {
+            _isPointerOverUI = EventSystem.current.IsPointerOverGameObject();
         }
     }
 }
