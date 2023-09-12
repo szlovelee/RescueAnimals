b8a0de648d1796a8fe4509067450caf12375f5e6
SongJinx3512 <subsub3512@gmail.com>
2023-09-12 11:40:22 +0900

    CharacterInput

---
+++ RescueAnimals/Assets/Scripts/RescueAnimalsChracterController.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RescueAnimalsCharacterController : MonoBehaviour
+{
+    // event 외부에서는 호출하지 못하게 막는다
+    public event Action<Vector2> OnMoveEvent;
+    public event Action<Vector2> OnLookEvent;
+    public event Action OnMagicShieldEvent;
+
+    private float _timeSinceLastAttack = float.MaxValue;
+
+    protected bool IsMagicShield { get; set; }
+
+
+    protected virtual void Update()
+    {
+        HandleAttackDelay();
+    }
+
+
+    private void HandleAttackDelay()
+    {
+        if (IsMagicShield && _timeSinceLastAttack > 0.2f)
+        {
+            _timeSinceLastAttack = 0;
+            CallMagicShieldEvent();
+        }
+    }
+
+
+    public void CallMoveEvent(Vector2 direction)
+    {
+        OnMoveEvent?.Invoke(direction);
+    }
+
+
+    public void CallLookEvent(Vector2 direction)
+    {
+        OnLookEvent?.Invoke(direction);
+    }
+
+    public void CallMagicShieldEvent()
+    {
+        OnMagicShieldEvent?.Invoke();
+    }
+}
